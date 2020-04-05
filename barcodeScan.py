efceb2176b0f93e5193d56dc7541324a6b42f775
Dirk Jansen <dj.jansen@casema.nl>
2020-04-05 09:33:13 +0200

    Update barcodeScan.py

--- barcodeScan.py
+++ barcodeScan.py
@@ -430,7 +430,7 @@ def barcodeScan(m_email, mret, mlist):
             self.q1Edit.setFont(QFont("Arial", 12))
             self.q1Edit.setFixedSize(155, 30)
             self.q1Edit.setFocus(True)
-            reg_ex = QRegExp("^[8]{1}[0-9]{12}$")
+            reg_ex = QRegExp("^[0-9]{13}$")
             input_validator = QRegExpValidator(reg_ex, self.q1Edit)
             self.q1Edit.setValidator(input_validator)
             self.q1Edit.returnPressed.connect(lambda: set_barcodenr(self, mlist))
