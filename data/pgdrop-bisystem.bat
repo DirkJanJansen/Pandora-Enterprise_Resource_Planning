c72b8d29721f280dff04664f15ffe7acf94f9ef2
Dirk Jansen <dj.jansen@casema.nl>
2020-05-25 09:53:39 +0200

    Update pgdrop-bisystem.bat

--- data/pgdrop-bisystem.bat
+++ data/pgdrop-bisystem.bat
@@ -1,4 +1,14 @@
 @echo off
 set PGPASSWORD=postgres45
+echo.
+echo.
+echo Pay Attention !!! The database is going to be removed.
+echo.
+echo.
+echo Press any key to remove the database bistem
+echo.
+echo.
+echo press  Ctrl+C to cancel
+pause > nul
 
 "C:\programData\postgres\bin\dropdb.exe"  -h localhost -p 5432 -U postgres  -w bisystem 
