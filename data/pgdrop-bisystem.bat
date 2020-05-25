dbbb96a10b3a995395db3dc4e046abd3a20df023
Dirk Jansen <dj.jansen@casema.nl>
2020-05-25 10:03:09 +0200

    Update pgdrop-bisystem.bat

--- data/pgdrop-bisystem.bat
+++ data/pgdrop-bisystem.bat
@@ -8,7 +8,7 @@ echo.
 echo Press any key to remove the database bistem
 echo.
 echo.
-echo press  Ctrl+C to cancel
+echo Or press  Ctrl+C to cancel
 pause > nul
 
 "C:\programData\postgres\bin\dropdb.exe"  -h localhost -p 5432 -U postgres  -w bisystem 
