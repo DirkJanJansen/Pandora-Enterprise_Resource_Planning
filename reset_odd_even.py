9bd91bb663d45e38cd9a0c8ab02bf4475932c7f4
Dirk Jansen <dj.jansen@casema.nl>
2023-03-17 17:11:53 +0100

    Add files via upload

--- reset_odd_even.py
+++ reset_odd_even.py
@@ -1,6 +1,17 @@
-import datetime
+import datetime, sys
 from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine,\
                      Float, update
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMessageBox, QApplication
+
+def odd_even():
+    msg = QMessageBox()
+    msg.setStyleSheet("font: 10pt Arial; color: black;  background-color: gainsboro")
+    msg.setWindowIcon(QIcon('./images/logos/logo.jpg'))
+    msg.setIcon(QMessageBox.Information)
+    msg.setText('Reset parameter successful!')
+    msg.setWindowTitle('Reset parameter')
+    msg.exec_()
 
 metadata = MetaData()
 params = Table('params', metadata,
@@ -14,3 +25,10 @@ con = engine.connect()
 mjaar = int(str(datetime.date.today())[0:4])
 updeven = update(params).where(params.c.paramID == 99).values(tarief = int(mjaar%2))
 con.execute(updeven)
+app = QApplication(sys.argv)
+app.setStyle("Windows")
+sys.exit(odd_even())
+app.exec_()
+
+
+
