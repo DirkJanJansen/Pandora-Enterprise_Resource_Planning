d80dd5f2c52d591da22a314353093493c7b77cf7
Dirk Jansen <dj.jansen@casema.nl>
2023-12-03 18:27:31 +0100

    Add files via upload

--- login.py
+++ login.py
@@ -719,10 +719,6 @@ def hoofdMenu(m_email):
                 mplist[15].model().item(2).setEnabled(False)
                 mplist[15].model().item(2).setForeground(QColor('darkgrey'))
                 mplist[15].model().item(2).setBackground(QColor('gainsboro'))
-            if mp[5][5] == '0':
-                mplist[15].model().item(3).setEnabled(False)
-                mplist[15].model().item(3).setForeground(QColor('darkgrey'))
-                mplist[15].model().item(3).setBackground(QColor('gainsboro'))
             if mp[3][5] == '0':
                 mplist[15].model().item(3).setEnabled(False)
                 mplist[15].model().item(3).setForeground(QColor('darkgrey'))
@@ -760,14 +756,6 @@ def hoofdMenu(m_email):
                 mplist[15].model().item(11).setForeground(QColor('darkgrey'))
                 mplist[15].model().item(11).setBackground(QColor('gainsboro'))
             if mp[3][5] == '0':
-                mplist[15].model().item(12).setEnabled(False)
-                mplist[15].model().item(12).setForeground(QColor('darkgrey'))
-                mplist[15].model().item(12).setBackground(QColor('gainsboro'))
-            if mp[7][5] == '0':
-                mplist[15].model().item(12).setEnabled(False)
-                mplist[15].model().item(12).setForeground(QColor('darkgrey'))
-                mplist[15].model().item(12).setBackground(QColor('gainsboro'))
-            if mp[9][5] == '0':
                 mplist[15].model().item(12).setEnabled(False)
                 mplist[15].model().item(12).setForeground(QColor('darkgrey'))
                 mplist[15].model().item(12).setBackground(QColor('gainsboro'))
