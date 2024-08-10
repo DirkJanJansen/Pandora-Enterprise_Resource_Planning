cc544834f5913a9bb443abf77a0f29b46b7381d3
Dirk Jansen <dj.jansen@casema.nl>
2024-08-10 23:22:20 +0200

    Add files via upload
    Added script for empty backup (only functional data)

---
+++ data/pgdump-custom.empty.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set PGPASSWORD=postgres45
+
+echo.
+echo.
+echo.
+echo The present  empty backup is going to be overwritten
+echo To preserve the present empty backup, save it in another folder or rename it!!
+echo Press any key to backup empty database bisystem.
+pause > nul
+echo.
+
+"C:\programdata\postgres\bin\pg_dump.exe" -U postgres -v --verbose -d  bisystem -Fc -f  "D:\Programming\Pandora\data\bisystem.empty.backup"
+echo.
+echo.
+echo Backup of the empty database bisystem is saved.
+echo.
+echo Press any key to close.
+pause > nul
+#-Fp Plain
+#-Fc Custom
+#-Fd directory
+#-Ft tar
--- data/pgrestore-custom.empty.bat
+++ data/pgrestore-custom.empty.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Restore database bisystem by bisystem.backup
+echo Restore empty database bisystem by bisystem.empty.backup
 
 set PGPASSWORD=postgres45
 echo.
@@ -9,19 +9,19 @@ echo First remove database bisystem with pgdrop-bisystem.bat
 echo Ctrl+C to cancel
 echo.
 echo.
-echo You are going to restore the database bisystem now!
+echo You are going to restore the empty database bisystem now!
 echo Press any key to restore database bisystem ........
 pause > nul
 echo.
 echo.
-echo Restore database bisystem by bisystem.backup
+echo Restore database bisystem by bisystem.empty.backup
 echo.
 echo.
 echo.
 
 "C:\programdata\postgres\bin\createdb.exe"  -h localhost -p 5432 -U postgres -w bisystem
 
-"C:\programdata\postgres\bin\pg_restore.exe" --dbname=bisystem  --verbose C:\programdata\postgres\data\bisystem.backup
+"C:\programdata\postgres\bin\pg_restore.exe" --dbname=bisystem  --verbose C:\programdata\postgres\data\bisystem.empty.backup
 
 echo.
 echo Database bisystem is restored
