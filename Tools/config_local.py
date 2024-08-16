808c386c36650aa8d86a3ecaeecaf428d9b0e4b3
Dirk Jansen <dj.jansen@casema.nl>
2024-08-16 12:37:03 +0200

    Update config_local.py

--- Tools/config_local.py
+++ Tools/config_local.py
@@ -1,4 +1,4 @@
-# Copy in C:\ProgramData\postgres\pgadmin 4\web (voor loon without password and no reminder for upgrade)
+# Copy in C:\ProgramData\postgres\pgadmin 4\web (voor logon without password and no reminder for upgrade)
 
 MASTER_PASSWORD_REQUIRED=False
 UPGRADE_CHECK_ENABLED=False
